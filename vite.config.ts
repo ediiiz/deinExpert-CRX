4878083e86da5a07b5a2a3ff890566cdcb0937a2
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-14 10:11:42 +0200

    add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { crx } from "@crxjs/vite-plugin";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 import manifest from "./src/manifest.config";
+import UnoCSS from '@unocss/svelte-scoped/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte(), crx({ manifest })],
+    plugins: [UnoCSS(), svelte(), crx({ manifest })],
     // HACK: https://github.com/crxjs/chrome-extension-tools/issues/696
     // https://github.com/crxjs/chrome-extension-tools/issues/746
     server: {
