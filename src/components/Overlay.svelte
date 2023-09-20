178b962cb82d74645b107148e34a47eea02c726b
EDIIIZ <ediz.tefenlilioglu@gmail.com>
2023-09-20 13:14:52 +0200

    display logic for the app

--- src/components/Overlay.svelte
+++ src/components/Overlay.svelte
@@ -9,18 +9,20 @@
   let state: modalState = 'close';
 </script>
 
-<div class="fixed bottom-4 left-0 z-9999 text-white w-screen">
-  <div class="flex place-content-center">
-    <button class="p-4 bg-dark rounded-4 shadow-dark shadow-2xl" on:click={switchModal}>
-      <div class="i-fluent-mdl2-collapse-menu text-2xl" />
-    </button>
+{#if window.location.pathname.startsWith('/shop')}
+  <div class="fixed bottom-4 left-0 z-9999 text-white w-screen">
+    <div class="flex place-content-center">
+      <button class="bg-white p-2 rounded-2 border-2 border-primary" on:click={switchModal}>
+        <div class="i-noto-man-detective-medium-light-skin-tone text-5xl" />
+      </button>
+    </div>
   </div>
-</div>
+{/if}
 
 {#if state == 'open'}
   <div transition:fade={{ duration: 200 }}>
     <div
-      class="fixed top-10 right-0 z-9999 bg-white text-black w-screen h-500px border-2 rounded-2 p-4 sm:max-w-sm sm:right-6 md:max-w-md 3xl:max-w-2xl"
+      class="fixed top-20 right-0 z-9999 bg-white text-black h-500px w-screen border-2 border-primary rounded-2 p-4 sm:max-w-sm sm:right-6 md:max-w-md 3xl:max-w-2xl"
     >
       <DeinExpert />
     </div>
